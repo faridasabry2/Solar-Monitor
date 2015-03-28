eb05ef18df6dc80c8a416466e4e07249f9aafc99
Barbarossa <apple@Barbarosa.local>
2015-03-28 12:38:33 -0400

    Initial Commit

---
+++ Solar Data/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Solar Data
+//
+//  Created by Barbarossa on 3/28/15.
+//  Copyright (c) 2015 SmirtEngineers for a Sustainable World. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Solar Data/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Solar Data
+//
+//  Created by Barbarossa on 3/28/15.
+//  Copyright (c) 2015 SmirtEngineers for a Sustainable World. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
