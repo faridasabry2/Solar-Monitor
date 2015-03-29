f034b839fc6ed1b01dbe47a5a9be01eb032d21f4
llilie <llilienkamp@smith.edu>
2015-03-29 09:04:40 -0400

    Adds in post request code, 1st version some errors

--- sparkeseswdata/sparkeseswdata.ino
+++ sparkeseswdata/sparkeseswdata.ino
@@ -1,3 +1,20 @@
+// This #include statement was automatically added by the Spark IDE.
+#include "HttpClient/HttpClient.h"
+
+HttpClient http;
+#define VARIABLE_ID "string"
+#define TOKEN "otherString"
+
+//Headers need to be set to init
+http_header_t headers[] = {
+    {"Content-Type", "application/json" },
+    {"X-Auth-Token", TOKEN },
+    { NULL, NULL }
+};
+
+http_request_t request;
+http_response_t response;
+
 int pinSolar = A0; 
 //pin monitoring power out
 int pinCharging = A1;
@@ -15,6 +32,10 @@ void setup() {
   
     pinMode( pinSolar, INPUT );
     pinMode( pinCharging, INPUT );
+    
+    request.hostname = "ourUrl"; //our server url
+    request.port = 80; 
+    //Serial.begin(9600);
 
 }
 int use[100] = {0}, generated[100] = {0}, i=0, j = 0;
@@ -50,6 +71,26 @@ void loop() {
         minutesNow = 0;
         
     }
+   
+    char [][] myArray = {
+        {"solarNow", String(solarNow) }, 
+        {"chargingNow", String(chargingNow) }, 
+        {"totalUse", String(totalUse)}, 
+        {"totalGenerated", String(totalUse}
+    };
+    
+    //Transmitting Data:
+    request.path = "/api/v1.6/variables/"VARIABLE_ID"/values";
+    request.body = "{\"value\":" + String(myArray) + "}";
+
+    http.post(request, response, headers);
+
+    //Serial.println(response.status);
+
+    //Serial.println(response.body);
+
+    //delay(1000);
     
     
 }
+
